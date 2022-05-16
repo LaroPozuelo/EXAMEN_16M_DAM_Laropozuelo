9692d71e305c493fd6ec84446a8ba3c0f53e0969
LaroPozuelo <lpozuelot@ceinmark.net>
2022-05-16 09:15:25 +0200

    Primer commit

---
+++ src/EXAMEN/Examen.java
@@ -0,0 +1,20 @@
+package EXAMEN;
+
+import java.util.Scanner;
+
+public class Examen {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Buenos días Ceinmark, soy Laro");
+		
+		Scanner input=new Scanner(System.in);
+		
+		System.out.println("Introduce un numero:");
+		int num=input.nextInt();
+		
+		
+
+	}
+
+}
