d0b79fe57ea5764dfa50ec64fab775f3c2a9bbb8
LaroPozuelo <lpozuelot@ceinmark.net>
2022-05-16 09:25:23 +0200

    segundo

--- src/EXAMEN/Examen.java
+++ src/EXAMEN/Examen.java
@@ -12,6 +12,20 @@ public class Examen {
 		
 		System.out.println("Introduce un numero:");
 		int num=input.nextInt();
+		boolean es=true;
+		
+		
+		for(int i = 2; i < num; i++) {
+	        if (num % i == 0) {
+	            System.out.println(num);
+	            es=false;
+	        }
+	    }
+		if(es) {
+			System.out.println("El numero es primo y solo es divisible entre 1 y el mismo.");
+		}else {
+			System.out.println("El numero no es primo es divisible entre todos los numeros anteriores y el 1 y el mismo.");
+		}
 		
 		
 
